85c12422b18e1176ed90462904b6cee766c79c4c
Umer Iftikhar <115613349+umeriplex@users.noreply.github.com>
2023-08-31 02:04:03 +0500

    start hive

--- lib/main.dart
+++ lib/main.dart
@@ -22,7 +22,7 @@ class MyApp extends StatelessWidget {
       ),
       home: AppTheme(
         data: AppThemeData.defaultWithSwatch(AppColors.red),
-        child: ButtonsView(),
+        child: HomePage(),
       ),
     );
   }
